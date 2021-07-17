a3124958564560b1f2a0403a8791922b222084c5
nathanvandeperre97 <nathanvandeperre@gmail.com>
2021-07-17 15:07:33 +0200

    feat(dio) : add provider service dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:riot_api_flutter/dio/riot_api_dio.dart';
 import 'package:riot_api_flutter/pages/account_page.dart';
 import 'package:riot_api_flutter/pages/main_menu_page.dart';
+import 'package:riot_api_flutter/providers/summoner_dto_provider.dart';
+import 'package:riot_api_flutter/services/summoner_dto_service.dart';
 
-void initServices() {}
+void initServices() {
+  GetIt.I.registerSingleton(RiotApiDao());
+  GetIt.I.registerSingleton(SummonerDtoService());
+  GetIt.I.registerSingleton(SummonerDtoProvider());
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
---
+++ lib/providers/summoner_dto_provider.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class SummonerDtoProvider with ChangeNotifier {}
