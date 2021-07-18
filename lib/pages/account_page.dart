425269b78772c933a0ba2d96016717097d9304df
nathanvandeperre97 <nathanvandeperre@gmail.com>
2021-07-18 02:20:19 +0200

    fix(date) : fix revisionDate format

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -151,8 +151,9 @@ class _AccountPageState extends State<AccountPage> {
   }
 
   Column printRevisionDate(SummonerDto summonerDto) {
+    var format = DateFormat('dd-MM-yyyy – HH:mm:ss');
     var date =
-        DateTime.fromMillisecondsSinceEpoch(summonerDto.revisionDate * 1000);
+        new DateTime.fromMillisecondsSinceEpoch(summonerDto.revisionDate);
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -163,7 +164,7 @@ class _AccountPageState extends State<AccountPage> {
                 fontSize: 16,
                 color: Color(0xfffbc02d),
                 decoration: TextDecoration.underline)),
-        Text(date.toString(),
+        Text(format.format(date),
             style: const TextStyle(fontSize: 16, color: Color(0xfffbc02d)))
       ],
     );
@@ -250,5 +251,34 @@ class _AccountPageState extends State<AccountPage> {
         }
       },
     );
+  }
+
+  String getDate(int timestamp) {
+    var now = DateTime.now();
+    var format = DateFormat('HH:mm a');
+    var date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+    var diff = now.difference(date);
+    var time = '';
+
+    if (diff.inSeconds <= 0 ||
+        diff.inSeconds > 0 && diff.inMinutes == 0 ||
+        diff.inMinutes > 0 && diff.inHours == 0 ||
+        diff.inHours > 0 && diff.inDays == 0) {
+      time = format.format(date);
+    } else if (diff.inDays > 0 && diff.inDays < 7) {
+      if (diff.inDays == 1) {
+        time = diff.inDays.toString() + ' DAY AGO';
+      } else {
+        time = diff.inDays.toString() + ' DAYS AGO';
+      }
+    } else {
+      if (diff.inDays == 7) {
+        time = (diff.inDays / 7).floor().toString() + ' WEEK AGO';
+      } else {
+        time = (diff.inDays / 7).floor().toString() + ' WEEKS AGO';
+      }
+    }
+
+    return time;
   }
 }
